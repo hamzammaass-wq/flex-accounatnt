fa49e7aa3f0b6bbd27c2e3d7fc0c296085a33242
اسمك على GitHub <  GitHub>
2026-05-25 16:25:10 +0300

    Fix Google Sign-In hang on iOS by adding GIDSignIn and Auth url handling to AppDelegate

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,6 +1,8 @@
 import UIKit
 import Capacitor
 import FirebaseCore
+import FirebaseAuth
+import GoogleSignIn
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -38,6 +40,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
         // Called when the app was launched with a url. Feel free to add additional processing here,
         // but if you want the App API to support tracking app url opens, make sure to keep this call
+        
+        if GIDSignIn.sharedInstance.handle(url) {
+            return true
+        }
+        
+        if Auth.auth().canHandle(url) {
+            return true
+        }
+
         return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
     }
 
