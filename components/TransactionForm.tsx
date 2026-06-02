1beed23414ad21472540ed9736a689dd8c50df1d
اسمك على GitHub <  GitHub>
2026-06-02 22:19:02 +0300

    fix: import missing useCallback in TransactionForm

--- components/TransactionForm.tsx
+++ components/TransactionForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo, useRef } from 'react';
+import React, { useState, useEffect, useMemo, useRef, useCallback } from 'react';
 import { createPortal } from 'react-dom';
 import * as XLSX from 'xlsx';
 import { useAccounting } from '../contexts/AccountingContext';
